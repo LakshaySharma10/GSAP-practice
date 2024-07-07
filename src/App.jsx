380703a1861a1a4a6bc115df2c2444d985a8fa86
LakshaySharma10 <lakshay7004@gmail.com>
2024-07-07 18:24:37 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Hero from './components/Hero'
+import Navbar from './components/Navbar'
+import Highlights from './components/Highlights'
+
+
+const App = () => {
+  return (
+    <main className='bg-black'>
+      <Navbar/>
+      <Hero/>
+      <Highlights/>
+
+    </main>
+  )
+}
+
+export default App
